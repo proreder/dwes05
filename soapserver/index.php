fb321511636fa1e11b659150ede410a289cd5805
proreder <proreder@hotmail.com>
2022-03-11 13:59:28 +0100

    ejercicio 1.4
    invocar crearReserva con SoapClient

--- soapserver/index.php
+++ soapserver/index.php
@@ -1,1 +1,19 @@
+<?php
 
+include_once 'logger.php';
+include_once 'ReservasSoapHandler.php';
+
+$url=strtolower(dirname($_SERVER['REQUEST_URI']));
+$uri="http://localhost/$url";
+
+//creamos las instacia de SoapServer y le pasamos el descriptor WDSL
+$server=new SoapServer("$uri/tarea5.wsdl");
+
+//Establecemos la clase descriptora de las peticiones SOAP a la clase 
+//"ReservasSoapHandle" 
+$server->setClass('ReservasSoapHandler');
+$server->handle();
+
+
+
+        
--- soapserver/verlog.php
+++ soapserver/verlog.php
@@ -1,1 +1,20 @@
+<?php
 
+//script para poder ver más cómodamente el contenido del archivo ws-errors.log
+include 'logger.php';
+
+header("refresh:5;url=verlog.php");
+if(isset($_GET['cleanlog'])){
+    header("LOCATION: verlog.php");
+}
+echo '<h3> Última llamada '.date('d-m-Y H:i:s').'</h3>';
+
+if(file_exists(ERROR_LOG_FILE)){
+    echo '<p style="text-align:right"><a href="?cleanlog">Limpiar Log</a</p>';
+    echo '<pre>';
+    readfile(ERROR_LOG_FILE);
+    echo'</pre>';
+    echo '<p style="text-align:right"><a href="?cleanlog">Limpiar Log</a></p>';
+}else{
+    echo '<h1>¡No hay fichero log!</h1>';
+}
