2dc3a3986b4872f64d1fbc76455cac6980582780
proreder <proreder@hotmail.com>
2022-03-13 13:45:58 +0100

    DWES Tarea 5

--- soapserver/src/utils.php
+++ soapserver/src/utils.php
@@ -325,3 +325,21 @@ function sePisa($nuevo_inicio, $nuevo_fin, $array):bool{
         }
         echo "</table>";
     }
+    
+    //funciones
+/**
+ * ejemplo: valida fecha y hora
+ * var_dump(validateDate('28/02/2012', 'd/m/Y')); # true
+ * var_dump(validateDate('30/02/2012', 'd/m/Y')); # false
+ * validaDate('14:50', 'H:i')); # true
+ * var_dump(validateDate('14:77', 'H:i')); # false
+ * @param type $date fecha a validar
+ * @param type $format  formato que tiene el tiempo
+ * @return false si hay error o tiempo de entrada si esta bién
+ */
+function validaDate($date, $format = 'Y-m-d H:i:s')
+{
+    $d = DateTime::createFromFormat($format, $date);
+    $valor= $d && $d->format($format) == $date;
+    return $valor== true ? $date : false;
+}
