ee4788593726d2b96a6118530fa12c816154e2ba
proreder <proreder@hotmail.com>
2022-03-13 21:48:49 +0100

    modificacion soaphelper

--- webapp/prueba.php
+++ webapp/prueba.php
@@ -7,3 +7,15 @@ echo $wsdluri;
 $client=new SoapClient($wsdluri, array('trace' => 1));
 var_dump($client->__getFunctions());
 
+$reserva=new class(){};
+$reserva->user=4;
+$reserva->zona=100;
+$reserva->horaInicio="10:00";
+$reserva->fecha='2022-03-01';
+$reserva->tramo= new class(){};
+$reserva->tramo->horaInicio="10:00";
+$reserva->tramo->horaFin="11:00";
+//$client->listarReservas('2022-03-01',1);
+
+var_dump($reserva);
+
