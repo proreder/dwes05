7fafc8d90dd67a04d9f64499aaba61b3e39ded41
proreder <proreder@hotmail.com>
2022-03-15 18:26:01 +0100

    Ejercicio 4 y modificacion de ReservasSoapHandler

--- soapserver/src/ReservasSoapHandler.php
+++ soapserver/src/ReservasSoapHandler.php
@@ -1,8 +1,11 @@
 <?php
 
+require_once 'utils.php';
+require_once 'Peticion';
 
 class ReservasSoapHandler{
     private $PDOconect;
+   
     //patrones de fecha y hora
     
     public function __construct() {
@@ -29,16 +32,56 @@ class ReservasSoapHandler{
      *       public $horaInicio;
      *       public horaFin;
      * 
-     * @return type
+     * @return $resultado int con el número de error encontrado
      */
     public function crearReserva($reserva){
+        $error=false;
+        $p=new Peticion();
         //logs de los datos recibidos para crear una reserva
         _log ('Datos recibidos:'.print_r($reserva, true));
-        $user=$reserva->user;
-        $zona=$reserva->zona;
-        $fecha=$reserva->fecha;
-        $horaInicio=$reserva->tramo->horaInicio;
-        $horaFin=$reserva->tramo->horaFin;
+        $_user=$reserva->user;
+        $_zona=$reserva->zona;
+        $_date=$reserva->fecha;
+        $_horaInicio=$reserva->tramo->horaInicio;
+        $_horaFin=$reserva->tramo->horaFin;
+        
+        //verificamo que los datos son válidos
+        //verificamos zona es numérico y positivo
+    //***zona**//
+    if(is_numeric($_zona) && $_zona=intval($zona)>0){
+        $zona=$_zona;
+    }else{
+        $error=-6;
+    }
+    //***User**//
+    if((is_numeric($_user) && $_user=intval($_user)>0) && !$error){
+        $user=$_user;
+    }else{
+        $error=-5;
+    }
+    //**fecha**
+     $date=$p->getString('fecha', true);
+     $_fecha=validaDate($date, 'Y-m-d');
+     
+     if(!$_fecha){
+         $error=-2;
+     }else{
+         $fecha=$_fecha;
+     }
+     //**horaInicio**
+     $_horaInicio=$p->getString('horaInicio', true);
+     $horaInicio=$p->validaDate($_horaInicio, 'H:i');
+     
+     if(!$horaInicio){
+         $errores[]="La hora de inicio no tiene el formato correcto";
+     }
+     //**horaFin**
+     $_horaFin=$p->getString('horaFin', true);
+     $horaFin=$p->validaDate($_horaFin, 'H:i');
+     if(!$horaFin){
+         $errores[]="La hora de inicio no tiene el formato correcto";
+     }
+        
         _log ('User: '.$user);
         _log ('Zona: '.$zona);
         _log ('Fecha: '.$fecha);
